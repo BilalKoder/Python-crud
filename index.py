cdf7ccef3dc96341a81ccbfeec7852394bdfa168
BilalKoder <bilalaly1122@gmail.com>
2023-02-06 04:07:24 +0500

    first init

--- index.py
+++ index.py
@@ -2,16 +2,17 @@ import mysql.connector
 import bcrypt
 
 
-#creating new database
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password=""
-# )
+#Creating new database
 
-# mycursor = mydb.cursor()
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
 
-# mycursor.execute("CREATE DATABASE pydoc")
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE pydoc")
 
 #connecting to database
 
@@ -24,48 +25,51 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+#creating table in database
+
+mycursor.execute("CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT,name VARCHAR(30) NOT NULL,email VARCHAR(60) NOT NULL,password VARCHAR(256) NOT NULL,PRIMARY KEY(id));")
+
+
+#Create
+
 # Declaring our password
 password = b'click123'
+
+#use pip install bcrypt
  
 # Adding the salt to password
 salt = bcrypt.gensalt()
 # Hashing the password
 hashed = bcrypt.hashpw(password, salt)
- 
-# printing the salt
-# print("Salt :")
-# print(salt)
- 
-# printing the hashed
-# print("Hashed")
-# print(hashed)
+
+sql = "INSERT INTO users (name,email,password) VALUES (%s, %s,%s)"
+val = ("Syed", "bilal.ali@koderlabs.com",hashed)
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
 
 
-#creating table in database
-# mycursor.execute("CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT,name VARCHAR(30) NOT NULL,email VARCHAR(60) NOT NULL,password VARCHAR(256) NOT NULL,PRIMARY KEY(id));")
+#Fetch
+
+mycursor.execute("SELECT * FROM users")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
 
 
-# sql = "INSERT INTO users (name,email,password) VALUES (%s, %s,%s)"
-# val = ("Syed", "bilal.ali@koderlabs.com",hashed)
-# mycursor.execute(sql, val)
+#Update 
 
-# mydb.commit()
+sql = "UPDATE users SET name = 'Ali' WHERE id = 1"
 
-# print(mycursor.rowcount, "record inserted.")
+mycursor.execute(sql)
 
-# mycursor.execute("SELECT * FROM users")
+mydb.commit()
 
-# myresult = mycursor.fetchall()
-
-# for x in myresult:
-#   print(x)
-
-
-# sql = "UPDATE users SET name = 'Ali' WHERE id = 1"
-
-# mycursor.execute(sql)
-
-# mydb.commit()
+#Delete 
 
 sql = "DELETE FROM users WHERE id = 1"
 
